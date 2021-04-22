71d4699dff0ebd1759d70d72d2e4b36f7030222f
froliceric0914 <wzhao.eric@gmail.com>
2021-04-21 22:38:47 -0400

    OOP practice: eclare Human and Earth Object

---
+++ Java_Object_Orientation_Practice/src/Earth.java
@@ -0,0 +1,26 @@
+
+public class Earth {
+	public static void main(String[] args) {
+		Human tom;
+		tom = new Human();
+		tom.name = "Tom";
+		tom.age = 5;
+		tom.eyeColor = "black";
+		tom.heightInInches = 32;
+		
+		tom.speak();
+		
+		Human eric;
+		eric = new Human();
+		eric.name="eric";
+		eric.heightInInches=70;
+		eric.eyeColor = "black";
+		eric.age= 30;
+		eric.speak();
+		
+		
+		
+	}
+}
+
+
---
+++ Java_Object_Orientation_Practice/src/Human.java
@@ -0,0 +1,22 @@
+
+public class Human {
+	
+	String name;
+	int age;
+	int heightInInches;
+	String eyeColor;
+	
+	//constructor method always as the same as the class
+	public Human() {
+		
+	}
+	
+	public void speak() {
+		System.out.println("My name is " + name);
+		System.out.println("My age is " + age);
+		System.out.println("My eye color is " + eyeColor);
+		System.out.println("My heightInInches is " + heightInInches);
+	}
+	
+	
+}
